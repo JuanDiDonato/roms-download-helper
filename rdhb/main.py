a103be4f6779990b37f011df7fc23c23f74c8c20
JuanDiDonato <juan_didonato@protonmail.com>
2026-03-05 16:31:47 -0300

    some fixes and update sources

--- rdhb/main.py
+++ rdhb/main.py
@@ -1,7 +1,9 @@
 import argparse
 import asyncio
+import os
 import sys
 from asyncio.subprocess import Process
+from pathlib import Path
 from typing import Optional
 from urllib.parse import unquote
 from urllib.parse import urljoin
@@ -15,6 +17,9 @@ parser.add_argument("--console", help="Console name from allowed console's list"
 parser.add_argument("--name", help="Game name filter", nargs="*", required=False)
 parser.add_argument("--exclude", help="Keywords to exclude", nargs="*", required=False)
 parser.add_argument("--language", help="Language's keyword", nargs="*", required=False)
+parser.add_argument("--path", help="Set a directory to save downloads", required=False, default=".")
+parser.add_argument("--override", help="Skip current roms validation", required=False, default=False)
+parser.add_argument("--check", help="Set a directory to check current roms", required=False, default=".")
 args = parser.parse_args()
 
 CONSOLE_SOURCE_MAP: dict[str, str] = {  # TODO: Update sources
@@ -22,7 +27,9 @@ CONSOLE_SOURCE_MAP: dict[str, str] = {  # TODO: Update sources
     "gamecube": "https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20[zstd-19-128k]/",
     "ps2": "https://myrient.erista.me/files/Redump/Sony%20-%20PlayStation%202/",
     "saturn": "https://myrient.erista.me/files/Internet%20Archive/chadmaster/chd_saturn/CHD-Saturn/Europe/",
-    "n64": "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%2064%20(ByteSwapped)/"
+    "n64": "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%2064%20(ByteSwapped)/",
+    "psv": "https://myrient.erista.me/files/Redump/Sony%20-%20PlayStation%20Portable/",
+    "dreamcast": "https://myrient.erista.me/files/Internet%20Archive/chadmaster/dc-chd-zstd-redump/dc-chd-zstd/"
 }
 
 async def show_loader() -> None:
@@ -34,11 +41,26 @@ async def show_loader() -> None:
         i += 1
         await asyncio.sleep(0.1)
 
+def raise_for_path() -> None:
+    if not os.path.exists(args.path) or not os.path.isdir(args.path):
+        raise Exception("Invalid path")
+
+def read_current_files() -> list[str]:
+    folder: Path = Path(args.check)
+    return [f.name for f in folder.iterdir() if f.is_file()]
+
+
 async def download(urls: list[str]) -> None:
     loader_task = asyncio.create_task(show_loader())
     try:
-        cmd: list[str] = ["aria2c", "-x", "4", "-s", "4", "--summary-interval=2",
-                          "--console-log-level=warn"] + urls
+        raise_for_path()
+        cmd: list[str] = ["aria2c",
+                          "-x", "4",
+                          "-s", "4",
+                          "-d", args.path if args.path else ".",
+                          "--show-console-readout=true",
+                          "--console-log-level=notice",
+                          "-Z"] + urls
 
         process: Process = await asyncio.create_subprocess_exec(
             *cmd,
@@ -55,6 +77,8 @@ async def download(urls: list[str]) -> None:
             print("Download successfully")
         else:
             print(f"Download fail: {stderr.decode().strip()}")
+    except Exception as e:
+        print(e.args)
     finally:
         loader_task.cancel()
         sys.stdout.write("\r" + " " * 30 + "\r")
@@ -81,7 +105,6 @@ def filter_roms(url: str, soup: BeautifulSoup) -> dict[str, str]:
                 filter_results[unquoted_filename] = full_url
     return filter_results
 
-
 async def main():
     if args.console:
         try:
@@ -93,11 +116,23 @@ async def main():
 
             # Find and filter urls
             scrapper_results: dict[str, str] = filter_roms(url, soup)
+            current_roms: list[str] = read_current_files()
 
+            if not args.override:
+                scrapper_results = dict(filter(lambda item: item not in current_roms, scrapper_results.items()))
+
+            roms_names: list[str] = list(scrapper_results.keys())
             if scrapper_results:
-                print(f"You will download: \n")
-                for rom in scrapper_results.keys():
-                    print(" - " + rom)
+                print(f"Search results: \n")
+                count: int = 0
+                for rom in roms_names:
+                    print(f"{count} - {rom}")
+
+                download_choices: list[str] = input("Select roms to download typing index separated by space (0 1 2 3..). Empty to cancel: ").split(" ")
+
+                selected_roms: dict[str, str] = {}
+                for choice in download_choices:
+                    selected_roms[roms_names[int(choice)]] = scrapper_results[roms_names[int(choice)]]
 
                 download_confirm: bool = (input("\n Start download? N/y: ") == "y")
                 if download_confirm:
